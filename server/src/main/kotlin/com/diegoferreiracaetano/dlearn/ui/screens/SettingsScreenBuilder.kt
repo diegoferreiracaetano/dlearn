92bb447acfdd5ee3ba35fca5bd64d5b4831f8d42
diegoferreiracaetano <di.cafe@hotmail.com>
2026-03-22 17:29:00 -0300

    settings

--- server/src/main/kotlin/com/diegoferreiracaetano/dlearn/ui/screens/SettingsScreenBuilder.kt
+++ server/src/main/kotlin/com/diegoferreiracaetano/dlearn/ui/screens/SettingsScreenBuilder.kt
@@ -1,11 +1,13 @@
 package com.diegoferreiracaetano.dlearn.ui.screens
 
 import com.diegoferreiracaetano.dlearn.ui.mappers.SettingsMapper
+import com.diegoferreiracaetano.dlearn.ui.sdui.AppContainerComponent
 import com.diegoferreiracaetano.dlearn.ui.sdui.AppStringType
 import com.diegoferreiracaetano.dlearn.ui.sdui.AppTopBarComponent
 import com.diegoferreiracaetano.dlearn.ui.sdui.Component
 import com.diegoferreiracaetano.dlearn.ui.sdui.Screen
 import com.diegoferreiracaetano.dlearn.util.I18nProvider
+import com.diegoferreiracaetano.dlearn.util.toAppContainerComponent
 
 class SettingsScreenBuilder(
     private val mapper: SettingsMapper,
@@ -13,43 +15,37 @@ class SettingsScreenBuilder(
 ) {
     fun buildNotificationScreen(lang: String): Screen {
         val components = mutableListOf<Component>()
-        
-        components.add(
-            AppTopBarComponent(
-                title = i18n.getString(AppStringType.NOTIFICATION_TITLE, lang)
-            )
+
+        val topbar = AppTopBarComponent(
+            title = i18n.getString(AppStringType.NOTIFICATION_TITLE, lang)
         )
-        
+
         components.addAll(mapper.toNotificationRows(lang))
-        
-        return Screen(components = components)
+
+        return  components.toAppContainerComponent(topbar)
     }
 
     fun buildLanguageScreen(currentLang: String): Screen {
         val components = mutableListOf<Component>()
-        
-        components.add(
-            AppTopBarComponent(
-                title = i18n.getString(AppStringType.LANGUAGE_TITLE, currentLang)
-            )
+
+        val topbar = AppTopBarComponent(
+            title = i18n.getString(AppStringType.LANGUAGE_TITLE, currentLang)
         )
         
         components.addAll(mapper.toLanguageRows(currentLang))
-        
-        return Screen(components = components)
+
+        return  components.toAppContainerComponent(topbar)
     }
 
     fun buildCountryScreen(currentCountry: String?, lang: String): Screen {
         val components = mutableListOf<Component>()
-        
-        components.add(
-            AppTopBarComponent(
-                title = i18n.getString(AppStringType.COUNTRY_TITLE, lang)
-            )
+
+        val topbar = AppTopBarComponent(
+            title = i18n.getString(AppStringType.COUNTRY_TITLE, lang)
         )
         
         components.addAll(mapper.toCountryRows(currentCountry, lang))
-        
-        return Screen(components = components)
+
+        return  components.toAppContainerComponent(topbar)
     }
 }

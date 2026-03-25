e33d49aeedf25048fdf37cdeca49ef6e545a7f32
diegoferreiracaetano <di.cafe@hotmail.com>
2026-03-25 10:03:36 -0300

    create user

--- shared/src/commonMain/kotlin/com/diegoferreiracaetano/dlearn/di/AuthModule.kt
+++ shared/src/commonMain/kotlin/com/diegoferreiracaetano/dlearn/di/AuthModule.kt
@@ -1,11 +1,12 @@
 package com.diegoferreiracaetano.dlearn.di
 
 import com.diegoferreiracaetano.dlearn.data.auth.challenge.OtpChallengeHandler
+import com.diegoferreiracaetano.dlearn.data.auth.challenge.remote.ChallengeRepositoryRemote
 import com.diegoferreiracaetano.dlearn.domain.auth.challenge.ChallengeCoordinator
 import com.diegoferreiracaetano.dlearn.domain.auth.challenge.ChallengeEngine
 import com.diegoferreiracaetano.dlearn.domain.auth.challenge.ChallengeHandler
+import com.diegoferreiracaetano.dlearn.domain.auth.challenge.ChallengeRepository
 import com.diegoferreiracaetano.dlearn.domain.session.SessionManager
-import com.diegoferreiracaetano.dlearn.ui.viewmodel.login.LoginViewModel
 import org.koin.dsl.module
 
 val authModule = module {
@@ -16,15 +17,12 @@ val authModule = module {
     single { ChallengeEngine(get(), getAll()) }
     
     // Handlers de Desafio
-    single<ChallengeHandler> { OtpChallengeHandler(get()) }
+    single { OtpChallengeHandler(get()) }
+    single<ChallengeHandler> { get<OtpChallengeHandler>() }
     // O BiometricChallengeHandler será injetado pelo platformAuthModule se disponível
 
-    factory {
-        _root_ide_package_.com.diegoferreiracaetano.dlearn.ui.viewmodel.login.LoginViewModel(
-            get(),
-            get()
-        )
-    }
+    // Repository
+    single<ChallengeRepository> { ChallengeRepositoryRemote(get(), get(), get()) }
 }
 
 expect val platformAuthModule: org.koin.core.module.Module

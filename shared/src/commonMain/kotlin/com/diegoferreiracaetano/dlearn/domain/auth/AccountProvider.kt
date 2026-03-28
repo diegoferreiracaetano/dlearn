2dec10540d9bb74bb09031244f3b24c564be57f8
diegoferreiracaetano <di.cafe@hotmail.com>
2026-03-28 17:17:45 -0300

    adcione provider account

--- shared/src/commonMain/kotlin/com/diegoferreiracaetano/dlearn/domain/auth/AccountProvider.kt
+++ shared/src/commonMain/kotlin/com/diegoferreiracaetano/dlearn/domain/auth/AccountProvider.kt
@@ -1,14 +1,16 @@
 package com.diegoferreiracaetano.dlearn.domain.auth
 
-import com.diegoferreiracaetano.dlearn.domain.user.MovieProvider
 import com.diegoferreiracaetano.dlearn.domain.user.User
 
 interface AccountProvider {
-    suspend fun saveAccount(user: User, provider: MovieProvider?, accessToken: String, refreshToken: String)
+    suspend fun saveAccount(
+        user: User, 
+        accessToken: String, 
+        refreshToken: String
+    )
     suspend fun getAccessToken(): String?
     suspend fun getRefreshToken(): String?
     suspend fun getUser(): User?
-    suspend fun getProvider(): MovieProvider?
     suspend fun clearAccount()
     suspend fun hasAccount(): Boolean
 }
